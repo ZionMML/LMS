aad05c8edc36ac1783c7742df2c9e16abf9d5868
Zion66682 <zion82@gmail.com>
2022-08-23 17:52:05 +0800

    Added Leave Summary logic

---
+++ ViewModels/SummaryViewModel.vb
@@ -0,0 +1,21 @@
+﻿Namespace ViewModels
+    Public Class SummaryViewModel
+
+        Public Property USER_ID As String
+
+        Public Property USER_NAME As String
+
+        Public Property LEAVE_TYPES As String
+
+        Public Property TOTAL_LEAVES_TAKEN As Decimal
+
+        Public Property ENTITLED_LEAVES As Decimal
+
+        Public Property LEAVE_BALANCE As Decimal
+
+        Public Property ISADMIN As Boolean
+
+    End Class
+End Namespace
+
+
